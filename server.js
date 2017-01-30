b90931d34380b4fd8eb62427c66fc82b833b4f2a
KatieAJenkins <kind2karma@gmail.com>
2017-01-30 13:32:23 -0700

    wired up front end so index.html is displaying

--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname,'app/public')));
+app.use(express.static(path.join(__dirname, '/../', 'node_modules')));
+
 const messages = require('./app/routes/classifieds');
 
 app.use('/classifieds',messages);
+
+app.use('*', function(req, res, next) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+});
 
 const port = process.env.PORT || 3000;
 
