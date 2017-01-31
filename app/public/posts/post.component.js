550c4f7c593092f4b558fe1e7dd01e6fe1a19d09
KatieAJenkins <kind2karma@gmail.com>
2017-01-30 19:41:59 -0700

    additional code cleanup for heroku deployment

--- app/public/posts/post.component.js
+++ app/public/posts/post.component.js
@@ -22,7 +22,6 @@
       function onInit(){
         // console.log("we made it to onInit");
         $http.get('/api/classifieds')
-
           .then(results => {
             vm.classifieds = results.data;
             // console.log(results.data);
@@ -31,19 +30,16 @@
 
       function submitNewPost (title, description, price, item_image) {
         var post = {title: title, description: description, price: price, item_image: item_image};
-
-        console.log(post);
-
+        // console.log(post);
         $http.post('/api/classifieds' , post)
           .then(response => {
             vm.classifieds.push(post);
             delete vm.post;
-          })
+          });
       }
 
       function setPropertyName(property){
         vm.sort = property;
       }
     }
-
 }());
