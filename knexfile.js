8c096f4377af4bb1ec900f4cd8af91b2ee319919
KatieAJenkins <kind2karma@gmail.com>
2017-02-03 14:23:28 -0700

    updated files for heroku knex issue

--- knexfile.js
+++ knexfile.js
@@ -4,18 +4,18 @@ module.exports = {
 
   development: {
     client: 'pg',
-    // connection: 'postgres://localhost/classifieds_dev'
-    connection: {
-      database: process.env.DATABASE_URL || 'classifieds_dev'
-    }
+    connection: 'postgres://localhost/classifieds_dev'
+    // connection: {
+    //   database: process.env.DATABASE_URL || 'classifieds_dev'
+    // }
   },
 
   test: {
     client: 'pg',
-    // connection: 'postgres://localhost/classifieds_test'
-    connection: {
-      database: process.env.DATABASE_URL || 'classifieds_test'
-   }
+    connection: 'postgres://localhost/classifieds_test'
+  //   connection: {
+  //     database: process.env.DATABASE_URL || 'classifieds_test'
+  //  }
  },
 
    production: {
